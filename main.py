3ab915767d84e1b8a94ecb5b9cba8a1dcbf1a957
scannon2k <seancannon2000@gmail.com>
2022-01-28 15:23:30 -0500

    first commit

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-#test
+#!/usr/bin/env python3
+from importlib.resources import path
+import os
+import shutil
+
+# Specify the filepath
+path = 'C:\\Users\\seanc\\Downloads'
+
+# Create a list of the files in the directory
+file_list = os.listdir(path)
+
+for file in file_list:
+    name, ext = os.path.splitext(file)
+
+    ext = ext[1:]
+
+    # Iterate if directory
+    if ext == '':
+        continue
+
+    # Move to extention folder or create new extention folder
+    if os.path.exists(path + '/' + ext):
+        shutil.move(path + '/' + file, path + '/' + ext + '/' + file)
+    else:
+        os.makedirs(path + '/' + ext)
+        shutil.move(path + '/' + file, path + '/' + ext + '/' + file)
+
+#try block for error handling
